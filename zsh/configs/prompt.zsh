6e4ca99a8861a15a65829d8a70b6869584bc4d44
Chad McKenna <chad.mckenna@welltok.com>
2016-06-13 10:12:02 -0500

    Updating gitconfig to welltok user and email, also updating prompt

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,9 +1,59 @@
 # modify the prompt to contain git branch name if applicable
-git_prompt_info() {
-  current_branch=$(git current-branch 2> /dev/null)
-  if [[ -n $current_branch ]]; then
-    echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
+if (( $+commands[git] ))
+then
+  git="$commands[git]"
+else
+  git="/usr/bin/git"
+fi
+
+ruby_version() {
+  if (( $+commands[rbenv] ))
+  then
+    echo "$(rbenv version | awk '{print $1}')"
+  fi
+
+  if (( $+commands[rvm-prompt] ))
+  then
+    echo "$(rvm-prompt | awk '{print $1}')"
   fi
 }
+
+rb_prompt() {
+  if ! [[ -z "$(ruby_version)" ]]
+  then
+    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
+  else
+    echo ""
+  fi
+}
+
+git_prompt_info() {
+  current_branch=$($git current-branch 2> /dev/null)
+  if [[ -n $current_branch ]]; then
+    echo "$current_branch"
+  fi
+}
+
+git_dirty() {
+  if $(! $git status -s &> /dev/null)
+  then
+    echo ""
+  else
+    if [[ $($git status --porcelain) == "" ]]
+    then
+      echo " %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+    else
+      echo " %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+    fi
+  fi
+}
+
+ssh_connection() {
+  echo "${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}"
+}
+
+directory_name() {
+  echo "%{$fg_bold[cyan]%}%c/%{$reset_color%}"
+}
 setopt promptsubst
-PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+PS1='$(ssh_connection)$(rb_prompt)$(directory_name)$(git_dirty) › '
