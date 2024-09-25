c91e73de4e73d8af619acac10fceb0452cebea96
Chad McKenna <chad.mckenna@hellotonic.com>
2024-09-25 14:56:14 -0600

    Adding a bunch of new goodies

--- config/nvim/lua/chadmckenna/remap.lua
+++ config/nvim/lua/chadmckenna/remap.lua
@@ -4,8 +4,10 @@ vim.keymap.set('n', '<Esc>', "<cmd> noh <CR>", {})
 vim.keymap.set('n', "<C-h>", "<C-w>h", {})
 vim.keymap.set('n', "<C-l>", "<C-w>l", {})
 vim.keymap.set('n', "<C-j>", "<C-w>j", {})
-vim.keymap.set('n', "<Cn-k>", "<C-w>k", {})
+vim.keymap.set('n', "<C-k>", "<C-w>k", {})
 vim.keymap.set('n', "<C-s>", "<cmd> w <CR>", {})
+
+vim.keymap.set('n', "gb", "<C-^>", {})
 
 vim.keymap.set('n', "]e", ":m .+1<CR>==", {})
 vim.keymap.set('n', "[e", ":m .-2<CR>==", {})
