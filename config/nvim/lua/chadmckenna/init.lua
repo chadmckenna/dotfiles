23f5fe14499cf4a1c28e6d60c2d6cab0633bdca8
Chad McKenna <chad.mckenna@hellotonic.com>
2024-09-26 07:43:21 -0600

    Adding imac specific stuff

--- config/nvim/lua/chadmckenna/init.lua
+++ config/nvim/lua/chadmckenna/init.lua
@@ -1,4 +1,20 @@
 require("chadmckenna.config.lazy")
 require("chadmckenna.remap")
 require("chadmckenna.set")
-require("nvim-tree").setup()
+
+require("nvim-tree").setup({
+  renderer = {
+    icons = {
+      show = {
+        git = false,
+        file = false,
+        folder = false,
+        folder_arrow = false
+      },
+      glyphs = {
+        symlink = ""
+      }
+    }
+  }
+})
+
