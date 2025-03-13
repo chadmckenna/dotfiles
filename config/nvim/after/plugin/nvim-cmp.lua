1faa729c119a5a3a6620e95cc41698b7359e87a8
Chad McKenna <chad.mckenna@hellotonic.com>
2025-03-13 09:02:01 -0600

    New completion shortcuts

--- config/nvim/after/plugin/nvim-cmp.lua
+++ config/nvim/after/plugin/nvim-cmp.lua
@@ -1,7 +1,11 @@
 local cmp = require('cmp')
 
 cmp.setup({
-    mapping = cmp.mapping.preset.insert({
-        ['<tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<Enter>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<esc>'] = cmp.mapping.abort()
+  }),
 })
+
