a5318d770cb371cd1aea360fab8cf711db010a43
Chad McKenna <chad.mckenna@hellotonic.com>
2025-06-30 15:17:16 -0600

    Updating to nvim 0.11, updating plugins too

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,31 +1,31 @@
-local lsp_zero = require('lsp-zero')
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
+  callback = function(event)
+    local opts = {buffer = event.buf}
 
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-
-  local opts = {buffer = bufnr, remap = false}
-
-  -- [d and ]d jump between lsp diagnostic messages
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gD", function()
-    vim.api.nvim_cmd({cmd = 'vsplit'}, {})
-    vim.lsp.buf.definition()
-  end, opts)
-  vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format() end, opts)
-end)
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "gD", function()
+      vim.api.nvim_cmd({cmd = 'vsplit'}, {})
+      vim.lsp.buf.definition()
+    end, opts)
+    vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format() end, opts)
+  end
 })
 
-lsp_zero.setup()
+require("mason").setup({
+  opts = {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗"
+      }
+    }
+  }
+})
+require("mason-lspconfig").setup({
+  ensure_installed = {},
+  automatic_installation = true,
+})
--- config/nvim/after/plugin/nvim-cmp.lua
+++ config/nvim/after/plugin/nvim-cmp.lua
@@ -1,11 +1,21 @@
 local cmp = require('cmp')
 
 cmp.setup({
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip', max_item_count = 5 },
+    -- { name = 'path', max_item_count = 5 },
+    { name = 'buffer', keyword_length = 5 },
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body);
+    end,
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-j>'] = cmp.mapping.select_next_item(),
     ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<Enter>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<esc>'] = cmp.mapping.abort()
   }),
 })
 
